2bf280b07d3bc4e46de4f8089c4866fa86adfb7a
VENKATESHA G S <venki354119@gmail.com>
2024-08-24 12:35:59 +0530

    arthmatic Operation

---
+++ src/ArthmaticOperationAdd.java
@@ -0,0 +1,21 @@
+public class ArthmaticOperationAdd {
+
+        public static void main(String[] args) {
+            ArthmaticOperationAdd a = new ArthmaticOperationAdd();
+
+            int result = a.add(10, 20);
+            System.out.println(result);
+
+
+        }
+
+        public int add(int x, int y) {
+            return x + y;
+        }
+
+
+    }
+
+
+
+
---
+++ src/ArthmaticOpertionDIv.java
@@ -0,0 +1,15 @@
+public class ArthmaticOpertionDIv {
+    public static void main(String[] args) {
+        ArthmaticOpertionDIv  a = new ArthmaticOpertionDIv();
+
+        double result = a.div(64, 27);
+        System.out.println(result);
+
+
+    }
+
+    public double div(double x, double y) {
+        return x / y;
+    }
+
+}
