0626f34c962f72635240e281e25093559f8d562f
VENKATESHA G S <venki354119@gmail.com>
2024-08-25 15:39:03 +0530

    Bank Account Details

---
+++ src/MethodOverloding.java
@@ -0,0 +1,26 @@
+public class MethodOverloding {
+
+    String name, city;
+    int age;
+
+    public MethodOverloding() {
+        this.age=age;
+        this.name=name;
+        this.city=city;
+    }
+
+    public static void main(String[] args) {
+    MethodOverloding m1 = new MethodOverloding();
+    m1.test("kashi",22,"");
+    }
+
+    public void test(String name, int age, String city) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+
+}
+
+
+
